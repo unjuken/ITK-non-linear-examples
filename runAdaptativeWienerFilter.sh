9ffcd3d7646d5250feb3f309bd2a8b47ced7c4b3
Andres Felipe Ramirez <unjuken@gmail.com>
2021-03-24 22:55:51 -0500

    modifying script

--- runAdaptativeWienerFilter.sh
+++ runAdaptativeWienerFilter.sh
@@ -1,5 +1,5 @@
-transformPath=./cmake-build-debug/AdaptativeWienerFilter
-folder=transformedImages
+transformPath=./build/AdaptativeWienerFilter
+folder=transformedImagesWiener
 mkdir "$folder"
 (
 cd $folder
@@ -7,31 +7,31 @@ mkdir archivos
 )
 for file in archivos/*;
 do
-  newPath="$folder/$file_1.3.nii"
+  newPath="$folder/$file\_1.3.nii"
   $transformPath $file $newPath 1 100
 done
 for file in archivos/*;
 do
-  newPath="$folder/$file_1.5.nii"
+  newPath="$folder/$file\_1.5.nii"
   $transformPath $file $newPath 1 300
 done
 for file in archivos/*;
 do
-  newPath="$folder/$file_1.7.nii"
+  newPath="$folder/$file\_1.7.nii"
   $transformPath $file $newPath 1 500
 done
 for file in archivos/*;
 do
-  newPath="$folder/$file_2.3.nii"
+  newPath="$folder/$file\_2.3.nii"
   $transformPath $file $newPath 2 100
 done
 for file in archivos/*;
 do
-  newPath="$folder/$file_2.5.nii"
+  newPath="$folder/$file\_2.5.nii"
   $transformPath $file $newPath 2 300
 done
 for file in archivos/*;
 do
-  newPath="$folder/$file_2.7.nii"
+  newPath="$folder/$file\_2.7.nii"
   $transformPath $file $newPath 2 500
 done
